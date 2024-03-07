2e1a51d6169d3506a955a5c5b0c71e84112c6bfc
Louiza Mak <louiza.mak@gmail.com>
2024-03-07 14:39:44 -0600

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
+
 function App() {
+  const [pepperoniIsClicked, setPepperoniIsClicked] = useState(false);
+
+  function togglePepperoni(event) {
+    setPepperoniIsClicked(event.target.checked);
+  }
+
   return (
-    <div></div>
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input 
+        type="checkbox"
+        id="pepperoni"
+        checked={pepperoniIsClicked}
+        aria-checked={pepperoniIsClicked}
+        onChange={togglePepperoni}
+      />
+      <label htmlFor="pepperoni">Add Pepperoni</label>
+
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepperoniIsClicked ? <li>Pepperoni</li> : null}
+      </ul>
+    </div>
   )
 }
 
